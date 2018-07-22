231f6c5f547691f74c0527fe019601859b74d6ea
Mark Zhu <markz@onemoretek.com>
2018-07-22 15:24:05 +0800

    added pthread supported

--- prebld.sh
+++ prebld.sh
@@ -1,5 +1,5 @@
 ./autogen.sh
-./configure --host=arm-fsl-linux-gnueabi --prefix=${PWD}/.install
+CFLAGS=-lpthread ./configure --host=arm-fsl-linux-gnueabi --prefix=${PWD}/.install
 make clean
 make 
 make install
